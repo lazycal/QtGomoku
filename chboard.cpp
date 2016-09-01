b7f87b1e0875d5dfa6d33557e3593d80add598f4
Lazycal <lazycal12@gmail.com>
2016-09-01 20:48:00 +0800

    add big endian

--- chboard.cpp
+++ chboard.cpp
@@ -191,6 +191,7 @@ void chBoard::readData()
     QByteArray buffer = tcpSocket->readAll();
     m_buffer.append(buffer);
     QDataStream in(m_buffer);
+    in.setByteOrder(QDataStream::BigEndian);
     while (m_buffer.size() >= sizeof(int)) {
         int bsz, type_id; Chess tmp; bool side;
         in >> bsz;
@@ -281,6 +282,7 @@ void chBoard::sendMessage()
 {
     QByteArray ba;
     QDataStream ds(&ba, QIODevice::WriteOnly);
+    ds.setByteOrder(QDataStream::BigEndian);
     ds << int(0) << int(0);
     ds << isHost << cs;
     ds.device()->seek(0);
@@ -292,6 +294,7 @@ void chBoard::sendMessage2(bool r[2])
 {
     QByteArray ba;
     QDataStream ds(&ba, QIODevice::WriteOnly);
+    ds.setByteOrder(QDataStream::BigEndian);
     ds << int(0) << int(1);
     ds << r[0] << r[1];
     ds.device()->seek(0);
